8d31cedf4aa24a84d9c8604d9452c9c33abb6149
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-05-24 04:40:46 -0400

    implement genre crud with mongodb

--- routers/genres.js
+++ routers/genres.js
@@ -2,59 +2,117 @@
 const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-let genres = [
-    {id: 1, name: 'Action'},
-    {id: 2, name: 'Adventure'},
-    {id: 3, name: 'Comedy'},
-    {id: 4, name: 'Crime'},
-    {id: 5, name: 'Drama'}
-];
+mongoose.connect('mongodb://localhost/movies')
+    .then(() => console.log("Connect to mongodb."))
+    .catch((err) => console.log("Something went worong", err));
+
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 router.get('/', (req, res) => {
-    res.send(genres);
+    getGenres()
+        .then(genres => res.send(genres))
+        .catch(err => res.status(500).send(err));    
 });
+
+async function getGenres() {
+    try{
+        return await Genre.find()
+                   .select({name: 1}); 
+    }catch(err) {
+        console.log(err);
+    }       
+}
 
 router.get('/:id', (req, res) => {
     const genreId = req.params.id;
-    const genre = genres.find(element => element.id === parseInt(genreId));
-    if(!genre) return res.status(404).send('The given id not found.');
-
-    res.send(genre);
+    
+    getGenre(genreId)
+        .then(genre => {
+            if(!genre) return res.status(404).send('The given id not found.');
+            res.send(genre)        
+        })
+        .catch(err => res.status(500).send(err));
 });
+
+async function getGenre(id) {
+    try{
+        return await Genre.findById(id)
+                        .select({name: 1});
+    }catch(err) {
+        console.log(err);
+    }
+}
 
 router.post('/', (req, res) => {
     const {error} = validateGenre(req.body);
 
     if(error) return res.status(400).send(error.details[0].message);
 
-    const genre = {id: genres.length + 1, name: req.body.name};
-    genres.push(genre);
-
-    res.send(genre);
+    createGenre(req.body.name)
+        .then(genre => res.send(genre))
+        .catch(err => res.status(500).send(err));   
 });
+
+async function createGenre(name) {
+    try{
+        let genre = new Genre({name: name});
+        return await genre.save();
+    }catch(err) {
+        console.log(err);
+    }    
+}
 
 router.put('/:id', (req, res) => {
     const {error} = validateGenre(req.body);
     if(error) return res.status(400).send(error.details[0].message);
     
     const genreId = req.params.id;
-    const genre = genres.find(genre => genre.id === parseInt(genreId));
-    if(!genre) return res.status(404).send('Genre not found with the given id.');
-    genre.name = req.body.name;
-
-    return res.send(genre);
+    updateGenre(genreId, req.body.name)
+        .then(newGenre => {
+            if(!newGenre) return res.status(404).send('Genre not found with the given id.');
+            res.send(newGenre);
+        })
+        .catch(err => res.status(500).send(err));
 });
+
+async function updateGenre(id, name) {
+    try{
+        return await Genre.findByIdAndUpdate(id, {
+            name: name
+        }, {new: true});     
+    }catch(err) {
+        console.log(err);
+    }
+}
 
 router.delete('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('Genre not found with the given id.');
+    const genreId = req.params.id;
 
-    var index = genres.indexOf(genre);
-    genres.splice(index, 1);
-
-    return res.send(genre);
+    deleteGenre(genreId)
+        .then(genre => {
+            if(!genre) return res.status(404).send('Genre not found with the given id.');
+            res.send(genre);
+        })
+        .catch(err => res.status(500).send(err)); 
 });
+
+async function deleteGenre(id) {
+    try{
+        return await Genre.findByIdAndRemove(id);
+    }catch(err) {
+        console.log(err);
+    }
+}
 
 function validateGenre(genre) {
     const schema = {
